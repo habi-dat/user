f0935a8e4aedd94008fad2c859c8ef3972f60806
Florian Humer <florian.humer@gmail.com>
2017-02-08 15:42:28 +0100

    fix mailing

--- utils/mailhelper/index.js
+++ utils/mailhelper/index.js
@@ -1,4 +1,4 @@
-var sendmail = require('sendmail');
+var nodemailer = require('nodemailer');
 var activation = require('../activation');
 var config = require('../../config/config.json');
 
@@ -14,17 +14,32 @@ exports.sendActivationEmail = function(user, done) {
 
             var link = config.settings.activation.base_url + '/passwd/' + user.uid + '/'+token.token;
             console.log('link: ' + link);
+            console.log('from: ' + (config.settings.activation.email_from?config.settings.activation.email_from:'no-reply@habidat.org'));
+            console.log('to: ' + user.mail);
+            console.log('html: ' + '<h1>Willkommen beim habiDAT!</h1>'+
+                      '<p>Dein Account wurde angelegt, bitte klicke auf den folgenden Link um dein Passwort zu wählen: </p>'+
+                      '<a href="'+ link +'">' + link + '</a>');
 
-            sendmail({
-                from: config.settings.activation.email_from?config.settings.activation.email_from:'no-reply@habidat.org',
+            var transport = nodemailer.createTransport(config.smtp);       
+
+
+            var mailOptions = {
+                from: (config.settings.activation.email_from?config.settings.activation.email_from:'no-reply@habidat.org'),
                 to: user.mail,
                 subject: 'Aktiviere deinen Account bei habiDAT',
                 html: '<h1>Willkommen beim habiDAT!</h1>'+
                       '<p>Dein Account wurde angelegt, bitte klicke auf den folgenden Link um dein Passwort zu wählen: </p>'+
                       '<a href="'+ link +'">' + link + '</a>'
-              }, function(err, reply) {
-                console.log('sent (maybe)');
-                done(err);
+            }
+
+            transport.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(error);
+                    done(error)
+                } else {
+                    console.log('Message %s sent: %s', info.messageId, info.response);
+                    done (error);
+                }
             });
         }
     })
