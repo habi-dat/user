f43b137d097a14763ac6641c133f8330cc59a035
soudis <florian.humer@gmail.com>
2018-04-10 10:42:36 +0200

    extend user account email notification

--- utils/mailhelper.js
+++ utils/mailhelper.js
@@ -21,11 +21,14 @@ exports.sendActivationEmail = function(user, done) {
                 from: (config.settings.activation.email_from?config.settings.activation.email_from:'no-reply@habidat.org'),
                 to: user.mail,
                 subject: 'Aktiviere deinen Account bei habiDAT',
-                html: '<h1>Willkommen beim habiDAT!</h1>'+
+                html: '<h3>Willkommen beim habiDAT!</h3>'+
                       '<p>Dein Account wurde angelegt, bitte klicke auf den folgenden Link um dein Passwort zu wählen: </p>'+
                       '<a href="'+ link +'">' + link + '</a>'+
                       '<p>Für Information zur Benutzung der Plattform bitte: </p> <a href="https://wiki.habidat.org/doku.php?id=benutzer_innenguide">HIER</a> klicken' +
-                      '<p>Für den Einstieg in die Plattform: </p><a href="https://cloud.habidat.org">cloud.habidat.org</a>'
+                      '<p>Für den Einstieg in die Plattform: </p><a href="https://cloud.habidat.org">cloud.habidat.org</a>' +
+                      '<p>Für Einstellungen zu deinem Account oder wennn du dein Passwort vergessen hast: </p><a href="https://user.habidat.org">user.habidat.org</a>' +
+                      '<p>Und für alle weiteren Fragen: </p><a href="mailto:support@xaok.org">support@xaok.org</a>' +
+                      '<p>Viel Spaß!</p>' 
             }
 
             transport.sendMail(mailOptions, (error, info) => {
@@ -57,7 +60,7 @@ exports.sendPasswordResetEmail = function(user, done) {
                 from: (config.settings.activation.email_from?config.settings.activation.email_from:'no-reply@habidat.org'),
                 to: user.mail,
                 subject: 'Passwort bei habiDAT zurückgesetzt',
-                html: '<h1>Dein Passwort wurde zurückgesetzt</h1>'+
+                html: '<h3>Dein Passwort wurde zurückgesetzt</h3>'+
                       '<p>Bitte klicke auf den folgenden Link um dein neues Passwort zu wählen: </p>'+
                       '<a href="'+ link +'">' + link + '</a>'
             }
