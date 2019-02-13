8fabbb2d8f254f68430d9ec9e1b8f1d9ed5b2768
Florian Humer <florian.humer@gmail.com>
2019-02-13 20:15:57 +0100

    bugfixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ -z "$HABIDAT_DOMAIN" ] && [ -z "$HABIDAT_USER_SUBDOMAIN" ] && [ -z "$HABIDAT_USER_LDAP_HOST" ] && [ -z "$HABIDAT_USER_LDAP_PORT" ] && [ -z "$HABIDAT_USER_LDAP_BINDDN" ] && [ -z "$HABIDAT_USER_LDAP_BASE" ] && [ -z "$HABIDAT_USER_LDAP_PASSWORD" ]
+if [ ! -f config/MANUAL_CONFIG ]
 then
   echo "Generating config.json"
   envsubst < config/config.json.sample > config/config.json
