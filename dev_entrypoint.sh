106f144c70611296561d7a7905c01690a227f6a5
Florian Humer <florian.humer@gmail.com>
2019-04-20 17:56:28 +0200

    bugfixes

--- dev_entrypoint.sh
+++ dev_entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -f config/MANUAL_CONFIG ]
+then
+  echo "Generating config.json"
+  envsubst < config/config.json.sample > config/config.json
+fi
+
 cd /app  
 npm install  
 ./node_modules/nodemon/bin/nodemon.js --legacy-watch app.js --ignore './data/'
