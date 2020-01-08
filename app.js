d63ce6fc8ef8e8b19f7ed521176fb7710f230eae
Florian Humer <florian.humer@gmail.com>
2020-01-08 22:49:46 +0100

    update dependencies and fix SAML SLO

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // dependencies
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
