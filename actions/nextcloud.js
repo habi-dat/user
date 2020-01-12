6d37efc7aac2ef95466a0f989be655d5f1df5814
Florian Humer <florian.humer@gmail.com>
2020-01-12 14:25:39 +0100

    fix nextcloud user provisioning by using API

--- actions/nextcloud.js
+++ actions/nextcloud.js
@@ -1,5 +1,6 @@
 var mysql       = require('mysql');
 var config      = require('../config/config.json');
+var request     = require('request-promise');
 var Promise   = require("bluebird");
 
 Promise.promisifyAll(require("mysql/lib/Connection").prototype);
@@ -51,24 +52,19 @@ var modifyUser = function(user) {
 
 // only if uid or dn changed: change uid and dn in LDAP mapping
 var createUser = function(user) {
-  return connectDb()
-    .then((connection) => {
-      var statement = "insert into " + config.nextcloud.db.prefix + "_ldap_user_mapping(ldap_dn, owncloud_name, directory_uuid) values ('" + user.dn.toLowerCase() + "','" + user.uid + "','" + user.uid + "')";
-      return connection.queryAsync(statement).then((results) => {
-        return connection.endAsync().then(() => {
-          return results;
-        });
-      }, (error) => {
-        connection.end();
-        throw error;
-      });
-    })
-    .then((results) => {
-      return {status : true, message: "NEXTCLOUD: Benutzer*innen-UID Zuordnung eingefügt (" + results.changedRows + ")"};
+  var options = {
+      uri: config.nextcloud.api.url + '/cloud/users?search=' + user.givenName + ' ' + user.surname,
+      headers: {
+          'OCS-APIRequest': 'true'
+      }
+  };
+  return request(options)
+    .then(() => {
+      return {status : true, message: "NEXTCLOUD: Benutzer*in provisioniert"};
     })
     .catch((error) => {
-      return {status: false, message: "NEXTCLOUD: Einfügen der Benutzer*innen-UID fehlgeschlagen: " + error};
-    });
+      return {status: false, message: "NEXTCLOUD: Benutzer*innenprovisionierung fehlgeschlagen: " + error};
+    });    
 };
 
 exports.register = function(hooks) {
